c65e635aa10cbcae59f0f5bf4367c98da3395839
John Cinnamond <john@cinnamond.me.uk>
2021-07-01 20:43:05 +0000

    Add subcommands to oto
    Default command is now to show the current person
    Add new command `next` to advance to the next person
    
    Refactored the code along the way

--- app/Main.hs
+++ app/Main.hs
@@ -5,47 +5,75 @@ module Main where
 import Control.Monad.State
 import Data.Time (UTCTime (utctDayTime), getCurrentTime)
 import Lib (shuffle)
+import System.Environment (getArgs)
 import System.IO.Strict (readFile)
 import Prelude hiding (readFile)
 
-data Names = Names
-    { idx :: Int
-    , names :: [String]
-    }
-
-run :: StateT Names IO ()
+run :: StateT OtoState IO ()
 run = do
     ns <- get
     ns' <- liftIO $ newNames ns
     put ns'
 
-newNames :: Names -> IO Names
-newNames n@Names{idx = i, names = ns} =
+newNames :: OtoState -> IO OtoState
+newNames c@OtoState{idx = i, names = ns} =
     if i == length ns - 1
         then do
             newNames <- shuffleNames ns
-            pure Names{idx = 0, names = newNames}
-        else pure n{idx = i + 1}
+            pure c{idx = 0, names = newNames}
+        else pure c{idx = i + 1}
 
-initialState :: IO Names
+initialState :: IO OtoState
 initialState = do
-    x <- readFile "people2"
+    let c = loadConfig
+    args <- getArgs
+    x <- readFile $ filepath c
     let (idxstr : names) = lines x
-    pure Names{idx = read idxstr, names = names}
+    pure OtoState{config = c, commands = args, idx = read idxstr, names = names}
 
-saveState :: Names -> IO ()
-saveState ns = do
-    let newNames = unlines $ show (idx ns) : names ns
-    writeFile "people2" newNames
+saveState :: OtoState -> IO ()
+saveState c = writeFile path newNames
+  where
+    newNames = unlines $ show (idx c) : names c
+    path = (filepath . config) c
 
 shuffleNames :: [String] -> IO [String]
 shuffleNames l = do
     seed <- floor . realToFrac . utctDayTime <$> getCurrentTime
     pure $ shuffle seed l
 
-main :: IO ()
-main = do
-    s <- initialState
-    putStrLn $ "The current person is: " ++ (names s !! idx s)
+newtype OtoConfig = OtoConfig
+    { filepath :: String
+    }
+
+type Name = String
+
+data OtoState = OtoState
+    { config :: OtoConfig
+    , commands :: [String]
+    , idx :: Int
+    , names :: [Name]
+    }
+
+loadConfig :: OtoConfig
+loadConfig = OtoConfig{filepath = "people2"}
+
+showCurrent :: OtoState -> IO ()
+showCurrent c = putStrLn $ "Current person is: " ++ (names c !! idx c)
+
+next :: OtoState -> IO ()
+next s = do
     ns <- execStateT run s
+    showCurrent ns
     saveState ns
+
+showUsage :: IO ()
+showUsage = putStrLn "usage: oto [next]"
+
+runCommand :: OtoState -> IO ()
+runCommand s@OtoState{commands = []} = showCurrent s
+runCommand s@OtoState{commands = ["next"]} = next s
+runCommand _ = showUsage
+
+main :: IO ()
+main = initialState >>= runCommand
